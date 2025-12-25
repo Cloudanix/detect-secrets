e193809288f1f9e5cc9db25c8d9d5beb8a15cbdc
Purusottam <mpurusottamc@gmail.com>
2025-12-24 16:44:04 -0800

    fixed version for cdx to avoid build errors

--- setup.py
+++ setup.py
@@ -1,67 +1,92 @@
-from pathlib import Path
+# import subprocess
+# from pathlib import Path
 
 from setuptools import find_packages
 from setuptools import setup
 
+from version import __version__
+
+
+# def get_version():
+#     """Parse package __version__.py to get version."""
+#     versionpy = (Path("detect_secrets") / "__version__.py").read_text()
+#     return versionpy.split("'")[1]
+
+
+# def get_version():
+#     try:
+#         version = (
+#             subprocess.check_output(
+#                 ["git", "describe", "--tags"],
+#                 stderr=subprocess.DEVNULL,
+#             )
+#             .decode()
+#             .strip()
+#         )
+#         if version:
+#             return version
+#     except Exception:
+#         pass
+
+#     return "0.0.0"
+
 
 def get_version():
-    """Parse package __version__.py to get version."""
-    versionpy = (Path('detect_secrets') / '__version__.py').read_text()
-    return versionpy.split("'")[1]
+    return __version__
 
 
 VERSION = get_version()
 
 
-with open('README.md', encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name='detect_secrets',
-    packages=find_packages(exclude=(['test*', 'tmp*'])),
+    name="detect_secrets",
+    packages=find_packages(exclude=(["test*", "tmp*"])),
     version=VERSION,
-    description='Tool for detecting secrets in the codebase',
+    description="Tool for detecting secrets in the codebase",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Yelp, Inc.',
-    author_email='opensource@yelp.com',
-    url='https://github.com/Yelp/detect-secrets',
-    download_url='https://github.com/Yelp/detect-secrets/archive/{}.tar.gz'.format(VERSION),
-    keywords=['secret-management', 'pre-commit', 'security', 'entropy-checks'],
+    long_description_content_type="text/markdown",
+    author="Yelp, Inc.",
+    author_email="opensource@yelp.com",
+    url="https://github.com/Yelp/detect-secrets",
+    download_url="https://github.com/Yelp/detect-secrets/archive/{}.tar.gz".format(VERSION),
+    keywords=["secret-management", "pre-commit", "security", "entropy-checks"],
     install_requires=[
-        'pyyaml',
-        'requests',
+        "pyyaml",
+        "requests",
     ],
     include_package_data=True,
     package_data={
-        'detect_secrets': [
-            'py.typed',
+        "detect_secrets": [
+            "py.typed",
         ],
     },
     extras_require={
-        'word_list': [
-            'pyahocorasick',
+        "word_list": [
+            "pyahocorasick",
         ],
-        'gibberish': [
-            'gibberish-detector',
+        "gibberish": [
+            "gibberish-detector",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'detect-secrets = detect_secrets.main:main',
-            'detect-secrets-hook = detect_secrets.pre_commit_hook:main',
+        "console_scripts": [
+            "detect-secrets = detect_secrets.main:main",
+            "detect-secrets-hook = detect_secrets.pre_commit_hook:main",
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Utilities',
-        'Environment :: Console',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Typing :: Typed',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Typing :: Typed",
     ],
 )
